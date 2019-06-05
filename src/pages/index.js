1dc3aad83b6a064f2bebf9710d1113e4c10ebfd1
Alfonso Roman <aroman@oit.ucla.edu>
2019-06-05 11:52:15 -0700

    updating for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  flex-direction: column;
+  height: 100%;
+
+  div {
+    font-size: 3rem;
+    line-height: 1;
+
+    &.small {
+      margin-left: 20px;
+      font-size: 2.2rem;
+    }
+  }
+
+`
 const IndexPage = () => (
   <Layout>
     <SEO title="Studiolino" />
-    <h1>スタディオリノ</h1>
+    <Main>
+      <div>ス</div><div>タ</div><div>デ</div><div className='small'>ィ</div><div>オ</div><div>リ</div><div>ノ</div>
+    </Main>
   </Layout>
 )
 
